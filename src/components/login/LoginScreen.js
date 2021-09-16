0f0cf8030f41958e556603174000e207148a2251
danielvillegasek <65374264+danielvillegasek@users.noreply.github.com>
2021-09-16 00:00:25 -0500

    Proteccion de rutas

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types';
 
 export const LoginScreen = ({history}) => {
 
+    const {dispatch} = useContext(AuthContext);
+
     const handleLogin = () => {
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
         // history.push('/');
-        history.replace('/');
+        // history.replace('/');
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Daniel'
+            }
+        });
+    
+        history.replace(lastPath);
     }
 
 
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Link, NavLink, useHistory } from 'react-router-dom'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types';
+
 
 export const Navbar = () => {
+
+    const { user, dispatch } = useContext(AuthContext);
+    const { name } = user;
+
+    const history = useHistory();
+
+    const handleLogout = () => {
+        dispatch({
+            type : types.logout
+        });
+        localStorage.removeItem('user');
+        history.replace('/login');
+       
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            
-            <Link 
-                className="navbar-brand" 
+
+            <Link
+                className="navbar-brand"
                 to="/"
             >
                 Asociaciones
@@ -15,27 +33,27 @@ export const Navbar = () => {
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                    <NavLink 
+                    <NavLink
                         activeClassName="active"
-                        className="nav-item nav-link" 
+                        className="nav-item nav-link"
                         exact
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
-                    <NavLink 
+                    <NavLink
                         activeClassName="active"
-                        className="nav-item nav-link" 
+                        className="nav-item nav-link"
                         exact
                         to="/dc"
                     >
                         DC
                     </NavLink>
 
-                    <NavLink 
+                    <NavLink
                         activeClassName="active"
-                        className="nav-item nav-link" 
+                        className="nav-item nav-link"
                         exact
                         to="/search"
                     >
@@ -46,14 +64,13 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+                    <span className="nav-item nav-link text-info"> {name} </span>
+                    <button
+                        className="btn nav-item nav-link"
+                        onClick={handleLogout}
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
